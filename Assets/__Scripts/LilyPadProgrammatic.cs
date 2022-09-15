bb4f31c54fb01546517471c1370c6e683d61c48a
Nosonic64 <s215585@students.aie.edu.au>
2022-09-15 15:29:59 +0930

    Changes to how the xylophone keys function

--- Assets/__Scripts/LilyPadProgrammatic.cs
+++ Assets/__Scripts/LilyPadProgrammatic.cs
@@ -40,7 +40,7 @@ public class LilyPadProgrammatic : MonoBehaviour
         // Parent the players transform to the lilypads so that they move with the platform
         if (col.gameObject.tag == "Player")
         {
-            col.gameObject.transform.parent = this.gameObject.transform;
+            col.gameObject.transform.parent.parent = this.gameObject.transform;
             playerOn = true;
         }
     }
@@ -59,7 +59,7 @@ public class LilyPadProgrammatic : MonoBehaviour
         // Unparent the player from the platform when they leave
         if (col.gameObject.tag == "Player")
         {
-            col.gameObject.transform.parent = null;
+            col.gameObject.transform.parent.parent = null;
             playerOn = false;
         }
     }
