68c2dcd1711170f4842c58634a492138c8bc5af4
300mice <ben@sacada.net>
2022-09-21 11:35:29 +1000

    added Bus code that redirects the mixer values to values that can be read by FMOD

---
+++ Assets/__Scripts/Bus.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Bus : MonoBehaviour
+{
+    FMOD.Studio.Bus bus;
+
+    [SerializeField]
+    private float busVolume;
+    public AudioMixer mixer;
+    private float ourVolume;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bus = FMODUnity.RuntimeManager.GetBus("bus:/Bus");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mixer.GetFloat("characterVolume", out ourVolume);
+        bus.setVolume(DecibelToLinear(ourVolume));
+    }
+
+    private float DecibelToLinear(float dB)
+    {
+        float linear = Mathf.Pow(10.0f, dB / 20f);
+        return linear;
+    }
+}
